e9aca45cf48788b6bfb28c5540adcb94f088fb22
AlexP <ppersn@gmail.com>
2017-07-24 23:02:03 +0300

    0.1.2 ver
    autoupdate node from github repo by rig-remote command

--- src/wrappers/stdout_miner_wrapper.js
+++ src/wrappers/stdout_miner_wrapper.js
@@ -86,6 +86,7 @@ class StdOutMinerWrapper extends i_miner_1.IMiner {
         });
         if (this.noTimestampCounter > 1 || this.hrTimestamp && now - this.hrTimestamp >= VALIDATION_LOOP_INTERVAL * 2) {
             debug('too much of inactivity, restarting process');
+            this.noTimestampCounter = 0;
             this.handleExit(666);
         }
         else {
